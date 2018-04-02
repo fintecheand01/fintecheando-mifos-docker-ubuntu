c7dffcb3ce940cf7cfebe56c8eb9ca88c85dfb00
Victor <devops@fintecheando.mx>
2018-04-01 23:23:51 -0500

    cambio en el hostname

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 keytool -genkey -noprompt -storepass password -keypass password -keyalg RSA -alias tomcat -dname "CN=tomcat" -keystore /usr/share/tomcat.keystore
 
-while ! mysqladmin ping -h"127.0.0.1" --silent; do
+while ! mysqladmin ping -h "127.0.0.1" --silent; do
     sleep 2
     echo "Waiting database..."
 done
