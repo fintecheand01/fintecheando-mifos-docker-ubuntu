e327d19b7b03217b049e6f490ec2641a7680a22a
Victor <devops@fintecheando.mx>
2018-04-05 17:14:22 -0500

    adding active MQ as a service

--- mifos/entrypoint.sh
+++ mifos/entrypoint.sh
@@ -2,10 +2,14 @@
 
 keytool -genkey -noprompt -storepass password -keypass password -keyalg RSA -alias tomcat -dname "CN=tomcat" -keystore /usr/share/tomcat.keystore
 
+while ! wget --retry-connrefused --waitretry=5 --read-timeout=20 --timeout=15 -t 10 -O /dev/null "http://admin:admin@sms-server:8161/admin/" ; do
+    sleep 2
+    echo "Waiting sms service..."
+done
+
 while ! mysqladmin --user=root --password=mysql ping -h "db-server" --silent; do
     sleep 2
     echo "Waiting database..."
 done
 
 catalina.sh run
-
