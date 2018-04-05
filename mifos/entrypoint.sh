6602be4f8b54ae73a400eb3d71152c05e88f9d0e
Victor <devops@fintecheando.mx>
2018-04-05 04:17:48 -0500

    changed the version to 18.03

--- mifos/entrypoint.sh
+++ mifos/entrypoint.sh
@@ -2,7 +2,7 @@
 
 keytool -genkey -noprompt -storepass password -keypass password -keyalg RSA -alias tomcat -dname "CN=tomcat" -keystore /usr/share/tomcat.keystore
 
-while ! mysqladmin --user=root --password=mysql ping -h "mariadb" --silent; do
+while ! mysqladmin --user=root --password=mysql ping -h "db-server" --silent; do
     sleep 2
     echo "Waiting database..."
 done
